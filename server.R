4a5124edda43c91a2d51cb1dd318f8a380d87d73
Liam Stewart <liams@umich.edu>
2024-10-26 14:53:15 -0400

    Bug fix to histogram label

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ yaxis <- c(state.name,"District of Columbia","Maine CD 1","Maine CD 2","Nebraska
 #function for generating histogram of all EV outcomes
 histGenerator <- function(){
   breaktest <- c(80,85,90,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,
-                 245,250,255,260,265,268.4,269.5,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,
+                 245,250,255,260,265,268.01,269.99,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,
                  360,365,370,375,380,385,390,395,400,405,410,415,420,425,430,435,440,445,450,455,460,465,470)
   gram <- hist(latestSimData$ElectoralVotes,breaks = breaktest,freq=TRUE)
   colorbreaks <- rep("firebrick1",length(gram$breaks))
